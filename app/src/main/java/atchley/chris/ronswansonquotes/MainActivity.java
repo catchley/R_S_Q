223f8a85e1e0a9ecc9850c51533b1cc993c3411f
Chris Atchley <steve.chris.atchley@gmail.com>
2015-11-24 19:18:48 -0500

    Initial commit

---
+++ app/src/main/java/atchley/chris/ronswansonquotes/MainActivity.java
@@ -0,0 +1,121 @@
+package atchley.chris.ronswansonquotes;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    public TextView mQuoteTextView;
+    public Button mRefreshButton;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mQuoteTextView = (TextView)findViewById(R.id.quoteTextView);
+        Typeface newFont = Typeface.createFromAsset(getAssets(), "fonts/livingst.ttf");
+        mQuoteTextView.setTypeface(newFont);
+        mRefreshButton = (Button)findViewById(R.id.refreshButton);
+        mRefreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getJoke();
+
+            }
+        });
+
+
+        getJoke();
+
+
+    }
+
+    private void getJoke() {
+        String swansonQuotesUrl ="http://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+
+        if(isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(swansonQuotesUrl)
+                    .build();
+
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    try {
+                        final String quote = response.body().string();
+                        final String quote1 =quote.replace("[", "").replace("]", "");
+                        Log.v(TAG, quote);
+                        if (response.isSuccessful()) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mQuoteTextView.setText(quote1);
+                                }
+                            });
+
+
+
+
+                        } else {
+                            alertUserAboutError();
+
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception Caught", e);
+                    }
+
+                }
+            });
+        }else{
+            Toast.makeText(this, R.string.network_unavailable_message, Toast.LENGTH_LONG).show();
+        }
+
+        Log.d(TAG, "Main UI code is running!");
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if(networkInfo != null && networkInfo.isConnected()){
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
+
+    }
+
+}
